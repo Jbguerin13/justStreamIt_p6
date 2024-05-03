b9d82555daf957b6a5bc40ae95007256a3975f69
jean-baptiste.guerin <jean-baptiste.guerin@jellyfish.com>
2024-05-04 00:41:12 +0200

    feat:best movie script, div access, setup button

--- web_python/script.js
+++ web_python/script.js
@@ -1,3 +1,6 @@
+const mainUrl = "http://localhost:8000/api/v1/"
+
+
 //function to fetch data from url APIrest
 function loadJsonFromUrl(url){
     fetch(url)
@@ -10,28 +13,42 @@ function loadJsonFromUrl(url){
 }
 
 //general function to fetch elem by id
-function fetchMovieById(jsonData){
+function getUrlMovieById(jsonData){
     let id = jsonData.results[0].id;
     let movieUrlByID = "http://localhost:8000/api/v1/" + id;
+    console.log(movieUrlByID);
     return movieUrlByID
 }
-
 
 
 //Best movie API request
 let bestMovieJsonUrl = "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score,-votes";
 
+getUrlMovieById(loadJsonFromUrl(bestMovieJsonUrl))
+
+
 //url to delete
 
-let jsonDataBestMovie = loadJsonFromUrl(bestMovieJsonUrl)
-console.log(jsonDataBestMovie)
+// let jsonDataBestMovie = loadJsonFromUrl(bestMovieJsonUrl)
+// console.log(jsonDataBestMovie)
 
 //let bestMovie = "http://localhost:8000/api/v1/titles/9008642"
 
 // Injection data in HTML Element Best Movie
 
-function bestFilmResultMainPage(result){
-    document.getElementById("bestMovie_image_url").innerHTML = "<img src=" + result.image_url + "alt='Best Film Image' height='400' width='400'/>";
-    document.getElementById("bestMovie_title").innerHTML = result.title;
-    document.getElementById("bestFilm__description").innerHTML = result.description;
+function bestFilmResultMainPage(jsonDataBestMovie){
+    document.getElementById("bestMovie_image_url")
+    .innerHTML = "<img src=" + jsonDataBestMovie.image_url + "alt='Best Film Image' height='400' width='400'/>";
+    document.getElementById("bestMovie_title")
+    .innerHTML = jsonDataBestMovie.title;
+    document.getElementById("bestFilm__description")
+    .innerHTML = jsonDataBestMovie.description;
   }
+
+
+// setup detailButton
+
+let detailButton = document.querySelectorAll('button')
+detailButton.addEventListener("click", function () {
+    console.log("ok button")
+});
